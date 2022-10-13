6ef5226bfb5a4ca34a377bfde0b2bacc2e86b646
Ithareal <ithareal.irl@gmail.com>
2022-10-13 19:21:10 +0200

    Blackjack

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,22 +1,46 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let message = ""
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-
-
-let numberEl = document.getElementById("number-el")
+let firstCard = 0
+let secondCard = 0
 let sum = 0
 
-function randomNumber() {
+let nextCard = 0
 
-    let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
-    let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
-    console.log(firstCard)
-    console.log(secondCard)
-    sum = firstCard + secondCard + "!"
-    numberEl.textContent = sum
-    console.log(sum)
+let hasBlackjack = false
+let isAlive = true
+
+firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
+secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
+sum = firstCard + secondCard + nextCard
+
+function startGame() {
+    renderGame()
+}
+function renderGame() {
+
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+
+    if (sum < 21) {
+        message = "Do you want a new card?"
+    }else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    nextCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
+    sum += nextCard
+    renderGame()
 }
 
 
