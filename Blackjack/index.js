e059800b63170786312ec245efb4e5e4966ebd1d
Ithareal <ithareal.irl@gmail.com>
2022-10-14 15:48:30 +0200

    .

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -3,24 +3,40 @@
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let message = ""
+let playerEl = document.getElementById("player-el")
 
-let firstCard = 0
-let secondCard = 0
+let message = ""
+let player = {
+    name: "Kim",
+    chips: 200
+}
+
+let cards = []
 let sum = 0
 
-let nextCard = 0
-
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 
-firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
-secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
-sum = firstCard + secondCard + nextCard
+playerEl.textContent = player.name + ": $" + player.chips
 
-let cards = [firstCard, secondCard]
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
     renderGame()
 }
 function renderGame() {
@@ -46,12 +62,12 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-    nextCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2
-    sum += nextCard
-    cards.push(nextCard)
-    console.log(cards)
-    renderGame()
+    if (isAlive === true && hasBlackjack === false) {
+        let nextCard = getRandomCard()
+        sum += nextCard
+        cards.push(nextCard)
+        renderGame()
+    }
 }
 
 
