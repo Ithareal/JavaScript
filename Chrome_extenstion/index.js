d3db30b77b3ecdfed69286101536544ad10201d4
Ithareal <ithareal.irl@gmail.com>
2022-10-20 17:52:17 +0200

    .

--- Chrome_extenstion/index.js
+++ Chrome_extenstion/index.js
@@ -1,1 +1,21 @@
-// https://scrimba.com/learn/learnjavascript/truthy-and-falsy-values-cKrLayTP
+let inputEl = document.getElementById("input-el")
+let buttonEl = document.getElementById("savelead-el")
+let leadsEl = document.getElementById("myleads-el")
+
+let myleads = []
+
+buttonEl.addEventListener("click", saveLead)
+
+function saveLead() {
+    myleads.push(inputEl.value)
+    inputEl.value = ""
+    console.log(myleads)
+    renderLeads()
+}
+
+function renderLeads() {
+    leadsEl.innerHTML = ""
+    for (i = 0; i < myleads.length; i++) {
+        leadsEl.innerHTML += `<li>${myleads[i]}</li>`
+    }
+}
---
+++ test/front_page.js
@@ -0,0 +1,11 @@
+let homeButtonEl = document.getElementById("home-el")
+let currentGameEl = document.getElementById("currentgame-el")
+let seedGeneratorEl = document.getElementById("seedgenerator-el")
+let settingsEl = document.getElementById("settings-el")
+
+currentGameEl.addEventListener("click", test)
+
+function test() {
+   location.href = "current_game.html"
+   console.log("hi")
+}
