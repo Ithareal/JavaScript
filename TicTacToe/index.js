582a671b3b5b98ddca527d49c0fe2fa8cbf64d5b
Ithareal <ithareal.irl@gmail.com>
2023-01-25 20:08:45 +0100

    .

---
+++ TicTacToe/index.js
@@ -0,0 +1,59 @@
+const slot1 = document.getElementById("slot1")
+const slot2 = document.getElementById("slot2")
+const slot3 = document.getElementById("slot3")
+const slot4 = document.getElementById("slot4")
+const slot5 = document.getElementById("slot5")
+const slot6 = document.getElementById("slot6")
+const slot7 = document.getElementById("slot7")
+const slot8 = document.getElementById("slot8")
+const slot9 = document.getElementById("slot9")
+
+// symbol = player ? 'X' : 'O'
+
+const btnReset = document.getElementById("btn1")
+const slots = [slot1, slot2, slot3, slot4, slot5, slot6, slot7, slot8, slot9]
+
+const player1Slots = []
+const player2Slots = []
+
+let player = true
+let symbol = ""
+const player1 = "Player"
+const player2 = "Computer" 
+
+function resetGame() {
+    for (i = 0; i < 9 ; i++) {
+        slots[i].innerHTML = "-"
+    }
+}
+
+function renderGame(slot) {
+    if (player == true){
+        symbol = "X"
+    } else {
+        symbol = "O"
+    }
+    console.log(slot)
+    if (slot.classList != "clicked") {
+        fillSlot(slot)
+        if (player == true) {
+            player1Slots.push(slot.id)
+            console.log(player1Slots)
+        } else {
+            player2Slots.push(slot.id)
+            console.log(player2Slots)
+        }
+        player =! player
+    if (player1Slots.includes("slot1" && "slot2" && "slot3") || player1Slots.includes("slot4" && "slot5" && "slot6")) {
+    btnReset.textContent = "YOU WIN"
+    if (confirm("NEW GAME?")) {
+        
+    }
+    }
+}
+    
+}
+function fillSlot(fillSlot) {
+    fillSlot.innerText = symbol
+    fillSlot.classList.toggle("clicked")
+}
